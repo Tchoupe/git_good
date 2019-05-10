01e327be8402c013a244ac0ec60c7277a850b2eb
Julien Drion <judrion@e-r8-p3.s19.be>
2019-05-10 14:50:39 +0200

    UPDATE\n\ncreate hello world some where

--- scripts/create_rand_stash.sh
+++ scripts/create_rand_stash.sh
@@ -2,8 +2,13 @@
 
 Y=$1
 RANDOM=$$;
+RANDOM2=$$;
 for i in `seq $2`
 do
+	if [ $i = $RANDOM2 % $Y]
+	then
+		echo $(($RANDOM2%Y)) >> random.txt;
+	fi
 	echo $(($RANDOM%Y)) >> random.txt;
 	git stash
 done
